4bc0e7b62eb2ae7b2a23f0585f86f8f907b55511
ryusuhan <ryusoohan@naver.com>
2018-11-16 18:41:51 +0900

    ASCII code very easy

---
+++ num11645.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class num11645 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        char A = s.next().charAt(0);
+
+        System.out.println(Integer.valueOf(A));
+
+
+
+
+    }
+}
