61d9f423a51b3fc5a4e03231209d640190dcf847
ryusuhan <ryusoohan@naver.com>
2018-11-16 18:29:11 +0900

    algorithm start

---
+++ aa.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class aa{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a=1;
+        for(int i = 1; i <=1001 ; i++ ){
+            a *= i;
+        }
+        System.out.println(a);
+
+    }
+}
