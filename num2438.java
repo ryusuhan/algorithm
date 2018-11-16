5355984ff7229b5bde8b2b52bf9de37b354099c2
ryusuhan <ryusoohan@naver.com>
2018-11-16 18:36:28 +0900

    print stars-1

---
+++ num2438.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class num2438 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
